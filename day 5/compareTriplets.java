30d5fd3fd9553655154bda17b115d41c3af8ac10
rameshsurabhi21 <72462152+rameshsurabhi21@users.noreply.github.com>
2020-10-22 13:30:56 +0530

    Add files via upload

---
+++ day 5/compareTriplets.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+     
+     List<Integer> out=new ArrayList<Integer>();
+      
+      int i=0;
+      int j=0;
+      for (int x=0;x<a.size();x++)
+      { if (a.get(x)>b.get(x))
+         {i=i+1; }
+        else if (a.get(x)<b.get(x))
+         {j=j+1; }
+         
+  
+         }
+         out.add(i);
+         out.add(j);
+ return out;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
